0c55480f789c3d515562c7e838183d9dbb64539b
Zach  Smith <zdsmith001@gmail.com>
2019-08-14 19:01:37 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,8 +1,8 @@
 # Make your shoe class here!
 class Shoe
+  attr_accessor :brand, :color, :size, :material, :condition
+
   def initialize(brand)
     @brand = brand
   end
-
-  attr_accessor :brand, :color, :size, :material, :condition
 end
