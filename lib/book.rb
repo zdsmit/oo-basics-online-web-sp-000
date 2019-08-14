20db978eca4a5dcb331d4b9d0e2464a95e144f21
Zach  Smith <zdsmith001@gmail.com>
2019-08-14 18:51:53 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,8 +1,9 @@
 class Book
   def initialize(title)
+    @title = title
   end
 
-attr_accessor :title
-attr_accessor :author
-attr_accessor :page_count
+  attr_accessor :title
+  attr_accessor :author
+  attr_accessor :page_count
 end
